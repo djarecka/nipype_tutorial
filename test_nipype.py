b9fd144db343b0801ab5aeab974555fe807b363c
Dorota Jarecka <djarecka@gmail.com>
2018-01-22 22:32:28 -0500

    checking if this is newest nipype

--- test_nipype.py
+++ test_nipype.py
@@ -4,3 +4,8 @@ def test_nipype():
     ver = nipype.__version__
     print("VER", ver)
     assert "dev" in ver
+
+def test_changes():
+    nn = nipype.Node
+    nn.is_cached
+    print(nn.is_cached)
